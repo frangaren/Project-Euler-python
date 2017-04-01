226d259964ce205c7cf8f9f98c02b05cd13b7407
Francisco García <frangaren@usal.es>
2017-04-01 16:36:46 +0200

    Cleanup in Problem 7

--- 007 - 10001st prime/main.py
+++ 007 - 10001st prime/main.py
@@ -3,6 +3,7 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
 # that the 6th prime is 13.
 # What is the 10 001st prime number?
+
 from itertools import islice
 from math import floor, sqrt
 
@@ -37,5 +38,6 @@ def primeCheck(n):
             return False
         f += 6
     return True
+
 if __name__ == "__main__":
     main()
