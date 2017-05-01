43a562185e2b39000d20369dca14ae61d7a466a1
Francisco García <frangaren@usal.es>
2017-05-01 13:45:02 +0200

    Problem 1 - Multiples of 3 and 5 rewritten

--- 001 - Multiples of 3 and 5/main.py
+++ 001 - Multiples of 3 and 5/main.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# coding: utf-8
+
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we
 # get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 def main():
-    print(sum(getMultiplesOf([3, 5], 1000)))
+    sum_multiples_below_10 = sum_multiples_of(3, 10) + \
+        sum_multiples_of(5, 10) - sum_multiples_of(15, 10)
+    sum_multiples_below_1000 = sum_multiples_of(3, 1000) + \
+        sum_multiples_of(5, 1000) - sum_multiples_of(15, 1000)
+    print(f"The multiples of 3 and 5 below 10 add up to " \
+        f"{sum_multiples_below_10}.")
+    print(f"The multiples of 3 and 5 below 1000 add up to " \
+        f"{sum_multiples_below_1000}.")
 
-def getMultiplesOf(numbers, maximum):
-    lst = list()
-    for n in numbers:
-        x = 1
-        m = x*n
-        while m < maximum:
-            if not (m in lst):
-                lst.append(x*n)
-            x += 1
-            m = x*n
-    #lst.sort()
-    return lst
+def sum_multiples_of(number, upper_bound):
+    multiple_count = (upper_bound - 1)//number
+    return (number * multiple_count * (multiple_count + 1)) // 2
 
 if __name__ == "__main__":
     main()
